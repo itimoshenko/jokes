4bdd71e6a90b8bc809b3aba3287eb5235b562856
Ivan Timoshenko <tis1993@mail.ru>
2023-04-25 19:13:55 +0200

    Added layout

---
+++ src/layout/Menu.tsx
@@ -0,0 +1,39 @@
+import React, { memo } from 'react';
+
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+import { Menu as AntMenu } from 'antd';
+
+const items = [
+  {
+    key: '/jokes',
+    label: 'Jokes',
+  },
+  {
+    key: '/favorites',
+    label: 'Favorites',
+  },
+];
+
+const Menu: React.FC = memo(() => {
+  const router = useRouter();
+
+  return (
+    <AntMenu
+      rootClassName="flex justify-center"
+      mode="horizontal"
+      selectedKeys={[router.route]}
+      items={
+        items.map((item) => ({
+          ...item,
+          label: (
+            <Link className="font-bold text-lg text-center inline-block w-[150px]" href={item.key}>{item.label}</Link>
+          ),
+        }))
+      }
+    />
+  );
+});
+
+export default Menu;
